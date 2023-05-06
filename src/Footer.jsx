81d12e4c4ff18cdec2fc2b7864c4de4a7c406ac0
yoNico21 <119441054+yoNico21@users.noreply.github.com>
2023-05-06 02:43:45 +0200

    big update

---
+++ src/Footer.jsx
@@ -0,0 +1,30 @@
+import "./scss/footer.scss";
+
+export function Footer() {
+  const date = "©" + " " + new Date().getFullYear();
+  const https = "https://";
+
+  const seagardenApp = "seagarden.app";
+  const seagarden = (
+    <>
+      <a href={https + seagardenApp}>{seagardenApp}</a>
+    </>
+  );
+
+  const mySite = "nielsnicola.xyz";
+  const nielsnicola = (
+    <>
+      <a href={https + mySite}>{mySite}</a>
+    </>
+  );
+
+  return (
+    <>
+      <section className="footer">
+        <p className="copyright">
+          {date} {seagarden} by {nielsnicola}
+        </p>
+      </section>
+    </>
+  );
+}
