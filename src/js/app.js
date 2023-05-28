5ef7c0c71d25d14ed8e71a194639d1a758d855fb
yoNico21 <119441054+yoNico21@users.noreply.github.com>
2023-05-28 02:43:51 +0200

    vercel analytics implementation

--- src/js/app.js
+++ src/js/app.js
@@ -1,32 +1,34 @@
-import 'destyle.css';
-import '/src/scss/app.scss';
-import { hero } from './hero';
-import { info } from './info';
-import { footer } from './footer';
-import { ar } from './ar';
+import "destyle.css";
+import "/src/scss/app.scss";
+import { hero } from "./hero";
+import { info } from "./info";
+import { footer } from "./footer";
+import { ar } from "./ar";
+import { inject } from "@vercel/analytics";
+inject();
 
-const app = document.querySelector('#app');
+const app = document.querySelector("#app");
 
 let showCanvas = false;
 
-if (app.innerHTML === '') display();
+if (app.innerHTML === "") display();
 
 function display() {
   if (showCanvas) {
     ar(app);
 
-    const share = document.querySelector('.btn-share');
-    const socials = document.querySelector('.socials-container');
+    const share = document.querySelector(".btn-share");
+    const socials = document.querySelector(".socials-container");
 
-    share.addEventListener('click', () => {
-      socials.classList.toggle('show');
-      console.log('click');
+    share.addEventListener("click", () => {
+      socials.classList.toggle("show");
+      console.log("click");
     });
 
-    const logo = document.querySelector('.logo-min');
-    logo.addEventListener('click', () => {
+    const logo = document.querySelector(".logo-min");
+    logo.addEventListener("click", () => {
       showCanvas = false;
-      app.innerHTML = '';
+      app.innerHTML = "";
       display();
     });
   } else {
@@ -34,12 +36,12 @@ function display() {
     info(app);
     footer(app);
 
-    const start = document.querySelector('.btn-start');
+    const start = document.querySelector(".btn-start");
 
     if (start) {
-      start.addEventListener('click', () => {
+      start.addEventListener("click", () => {
         showCanvas = true;
-        app.innerHTML = '';
+        app.innerHTML = "";
         display();
       });
     }
