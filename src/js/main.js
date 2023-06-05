a86f43e97e9ffbbf10921f45b35013c5f38ee90a
yoNico21 <119441054+yoNico21@users.noreply.github.com>
2023-06-05 03:48:50 +0200

    landing page update

--- src/js/main.js
+++ src/js/main.js
@@ -11,9 +11,8 @@ export function main(element) {
   add(mainContent, section);
 
   const content = [
-    { label: "Intro", function: intro() },
+    { label: "About", function: about() },
     { label: "Creatures", function: creatures() },
-    { label: "More", function: more() },
   ];
 
   content.forEach((item, i) => {
@@ -25,16 +24,69 @@ export function main(element) {
     div.addEventListener("click", () => {
       mainContent.innerHTML = "";
       add(content[i].function, mainContent);
+      for (let child of nav.children) {
+        if (child.classList.contains("active")) {
+          child.classList.remove("active");
+        }
+      }
+      div.classList.add("active");
     });
   });
+
+  nav.children[0].classList.add("active");
 
   add(content[0].function, mainContent);
   add(section, element);
 }
 
-function intro() {
-  let section = create("section", "intro");
+function about() {
+  let section = create("section", "about");
   section.classList.add("main-item");
+
+  const content =
+    "seagarden.app is a web-based augmented reality application. It acts as a second layer on top of Park Zypendaal and Sonsbeek located in Arnhem. Displaying imagery of a sea garden full of creatures to explore while going for a walk in the park. The start of this project is a reflection of the <i>Molukse Zeetuin</i> (which translates to Moluccan sea garden) which took place in Arnhem in 1928. The Moluccan sea garden can be interpreted as an early attempt at an augmented reality experience. This ultimately gave me the idea to create a modern-day sea garden for Park Zypendaal and Sonsbeek, in the form of a web application.";
+
+  let p = create("p", "project-description");
+  p.innerHTML = content;
+  add(p, section);
+
+  let img = create("img", "molukse-zeetuin");
+  img.src = "/zeetuin.jpg";
+  img.setAttribute(
+    "alt",
+    "A picture of a artificially created sea garden, dating back to 1928"
+  );
+  add(img, section);
+
+  let caption = create("p", "image-caption");
+  caption.innerHTML = `
+  <i>Molukse Zeetuin, Zijpendaalseweg Arnhem, 1928 – Photo: <a href="https://www.geldersarchief.nl/bronnen/foto-s-en-films?mivast=37&miadt=37&mizig=284&miview=gal&mizk_alle=molukse%20zeetuin" target="_blank">Gelders Archief</a></i>
+  `;
+  add(caption, section);
+
+  const githubParagraph =
+    "This web application is an art project and the result of a study assignment. People who are further interested in the development of this website can find everything in a publicly available GitHub repository. You can visit it by clicking the following button:";
+
+  let pGH = create("p", "github-description");
+  pGH.innerHTML = githubParagraph;
+  add(pGH, section);
+
+  let button = create("button", "btn");
+  button.classList.add("btn-github");
+  add(button, section);
+
+  let a = create("a", "repo-hyperlink");
+  a.href = "https://github.com/yoNico21/AR-Seagarden";
+  a.setAttribute("target", "_blank");
+  add(a, button);
+
+  let i = create("i", "fa-brands");
+  i.classList.add("fa-github");
+  add(i, a);
+
+  let span = create("span");
+  span.innerText = "yoNico21/AR-Seagarden";
+  add(span, a);
 
   return section;
 }
@@ -45,35 +97,3 @@ function creatures() {
 
   return section;
 }
-
-function more() {
-  let section = create("section", "more");
-  section.classList.add("main-item");
-
-  let p = create("p", "project-description");
-  p.innerText = content;
-
-  let button = create("button", "btn");
-  button.classList.add("btn-github");
-
-  let a = create("a", "repo-hyperlink");
-  a.href = "https://github.com/yoNico21/AR-Seagarden";
-  a.setAttribute("target", "_blank");
-
-  let i = create("i", "fa-brands");
-  i.classList.add("fa-github");
-
-  let span = create("span");
-  span.innerText = "yoNico21/AR-Seagarden";
-
-  add(p, section);
-  add(button, section);
-  add(a, button);
-  add(i, a);
-  add(span, a);
-
-  return section;
-}
-
-const content =
-  "seagarden.app is a web-based augmented reality application created by Niels Nicola as a result of an art school assignment. It acts as a second layer on top of Park Zypendaal and Sonsbeek located in Arnhem. Displaying imagery of a sea garden for people to explore while going for a walk in the park. If you want to take a look under the hood you can access the repository on GitHub.";
