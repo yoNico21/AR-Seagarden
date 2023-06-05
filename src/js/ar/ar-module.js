2b8a33ff22dac0265cc7f763f757d29bcf26549f
yoNico21 <119441054+yoNico21@users.noreply.github.com>
2023-06-05 23:15:44 +0200

    randomizing positions test

--- src/js/ar/ar-module.js
+++ src/js/ar/ar-module.js
@@ -24,18 +24,34 @@ export function startAR() {
   // Load in objects
   const objects = [
     // { fileName: "creature_1", lat: 5.9108008, lon: 51.9829402, scale: 1.0 }, // at home
-    { fileName: "creature_1", lat: 5.9109252, lon: 51.9828927, scale: 2.0 },
-    { fileName: "creature_2", lat: 5.9108725, lon: 51.9829071, scale: 2.0 },
-    { fileName: "creature_3", lat: 5.9108651, lon: 51.9829544, scale: 2.0 },
-    { fileName: "creature_4", lat: 5.910906, lon: 51.982916, scale: 2.0 },
+    { fileName: "creature_1", scale: 2.0 },
+    { fileName: "creature_2", scale: 2.0 },
+    { fileName: "creature_3", scale: 2.0 },
+    { fileName: "creature_4", scale: 2.0 },
   ];
 
+  let coordinates = [
+    { lat: 5.9109252, lon: 51.9828927 },
+    { lat: 5.9108651, lon: 51.9829544 },
+    { lat: 5.9108725, lon: 51.9829071 },
+    { lat: 5.910906, lon: 51.982916 },
+  ];
+  shuffle(coordinates);
+
+  let elevation = [{ level: 0 }, { level: 1 }, { level: 3 }, { level: 5 }];
+  shuffle(elevation);
+
   const loader = new GLTFLoader();
-  objects.forEach((object) => {
+  objects.forEach((object, i) => {
     loader.load(`/assets/${object.fileName}.glb`, function (glb) {
       const scale = object.scale;
       glb.scene.scale.setScalar(scale);
-      arjs.add(glb.scene, object.lat, object.lon);
+      arjs.add(
+        glb.scene,
+        coordinates[i].lat,
+        coordinates[i].lon,
+        elevation[i].level
+      );
     });
   });
 
@@ -57,10 +73,30 @@ export function startAR() {
 
     // Update the scene using the latest sensor readings
     deviceOrientationControls.update();
-    console.log(deviceOrientationControls.update());
 
     cam.update();
     renderer.render(scene, camera);
     requestAnimationFrame(render);
   }
+}
+
+// Fisher-Yates (Knuth) Shuffle, found on: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffle(array) {
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
 }
